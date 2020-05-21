12b6a6ba4606afcdb11f1590c9449f51ac2a4934
Jeff Jenkins <jjenkins120@gmail.com>
2020-05-21 19:34:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,11 @@
 
 
-def square_array(array)
-  
+def square_array(numbers)
+  result = array.new(numbers.size)
+  i = 0
+  while i < numbers.size
+    result[i] = numbers[i] ** 2
+    i += 1
+  end
+  result
 end
